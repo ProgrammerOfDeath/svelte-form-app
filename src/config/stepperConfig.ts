2c9991dd6bbbb61516b110ab5f284eb6e475ef3b
ProgrammerOfDeath <rene.hotho@gmail.com>
2023-02-23 23:19:59 +0100

    core: create a config for the stepper

---
+++ src/config/stepperConfig.ts
@@ -0,0 +1,20 @@
+import type { Step } from '../types/Step'
+
+export const steps: Step[] = [
+  {
+    title: 'Step 1',
+    subTitle: 'Your Info',
+  },
+  {
+    title: 'Step 2',
+    subTitle: 'Select Plan',
+  },
+  {
+    title: 'Step 3',
+    subTitle: 'Add-Ons',
+  },
+  {
+    title: 'Step 4',
+    subTitle: 'Summary',
+  },
+]
