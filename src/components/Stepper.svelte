18e6f81878bcf8980192ed5347b6a669666974f4
ProgrammerOfDeath <rene.hotho@gmail.com>
2023-02-23 23:19:25 +0100

    refactor: create seperate component for list item

--- src/components/Stepper.svelte
+++ src/components/Stepper.svelte
@@ -1,36 +1,12 @@
+<script lang="ts">
+import StepperItem from './StepperItem.svelte'
+import { steps } from '../config/stepperConfig'
+</script>
+
 <div class="rounded-lg py-9 px-8 bg-sidebar-desktop bg-cover bg-bottom bg-no-repeat">
   <ul class="list-none">
-    <li class="flex items-center mb-4">
-      <div class="rounded-full bg-primary-light-blue w-5 h-5 grid place-content-center mr-2">1</div>
-      <div class="uppercase">
-        <p class="text-xs text-neutral-cool-gray">Step 1</p>
-        <p class="text-sm font-medium text-neutral-white">Your Info</p>
-      </div>
-    </li>
-    <li class="flex items-center">
-      <div
-        class="rounded-full border-white border text-neutral-white w-5 h-5 grid place-content-center mr-2"
-      >
-        1
-      </div>
-      <div class="uppercase">
-        <p class="text-xs text-neutral-cool-gray">Step 1</p>
-        <p class="text-sm font-medium text-neutral-white">Your Info</p>
-      </div>
-    </li>
-    <li class="flex">
-      <div class="rounded-full bg-primary-pastel w-5 h-5 grid place-content-center mr-2">1</div>
-      <div class="uppercase">
-        <p class="text-xs text-neutral-cool-gray">Step 1</p>
-        <p class="text-sm font-medium text-neutral-white">Your Info</p>
-      </div>
-    </li>
-    <li class="flex">
-      <div class="rounded-full bg-primary-pastel w-5 h-5 grid place-content-center mr-2">1</div>
-      <div class="uppercase">
-        <p class="text-xs text-neutral-cool-gray">Step 1</p>
-        <p class="text-sm font-medium text-neutral-white">Your Info</p>
-      </div>
-    </li>
+    {#each steps as step, i}
+      <StepperItem stepTitle={step.title} stepContent={step.subTitle} stepNumber={i + 1} />
+    {/each}
   </ul>
 </div>
--- src/components/StepperItem.svelte
+++ src/components/StepperItem.svelte
@@ -1,7 +1,15 @@
+<script lang="ts">
+export let stepTitle: string = ''
+export let stepContent: string = ''
+export let stepNumber: number = 0
+</script>
+
 <li class="flex items-center mb-4">
-  <div class="rounded-full bg-primary-light-blue w-5 h-5 grid place-content-center mr-2">1</div>
+  <div class="rounded-full bg-primary-light-blue w-7 h-7 grid place-content-center mr-2">
+    {stepNumber}
+  </div>
   <div class="uppercase">
-    <p class="text-xs text-neutral-cool-gray">Step 1</p>
-    <p class="text-sm font-medium text-neutral-white">Your Info</p>
+    <p class="text-xs text-neutral-cool-gray">{stepTitle}</p>
+    <p class="text-sm font-medium text-neutral-white">{stepContent}</p>
   </div>
 </li>
